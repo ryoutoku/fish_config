39874f1c0811cf7a4bcf5711eb2d6a3ad7553406
Akinori Hayashi <hayashi@cognirobo.com>
2021-10-03 17:55:10 +0900

    feat: fish用alias

---
+++ config.fish
@@ -0,0 +1,40 @@
+alias vi='/usr/bin/vim'
+
+## for windows explorer (required win10 creators update)
+# alias open='explorer.exe'
+# alias e='explorer.exe .'
+
+# cd > ls
+function cd
+  builtin cd $argv
+    ls -a
+end
+
+# lsの色を見やすく変更
+if test ! -e ~/.dircolors/dircolors.ansi-dark
+    git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors
+end
+eval (dircolors -c ~/.dircolors/dircolors.ansi-dark)
+
+# aliases for git
+alias g="git"
+alias gs="git status"
+alias gc="git checkout"
+alias gd="git diff"
+alias ga="git add"
+alias gca="git commit -a -m"
+alias gcm="git commit -m"
+alias gbd="git branch -D"
+alias gp="git push"
+alias gb="git branch"
+alias gcob="git checkout -b"
+alias gco="git checkout"
+alias gba="git branch -a"
+alias glo="git log --graph --date=iso --pretty='[%ad]%C(auto) %h%d %Cgreen%an%Creset : %s'"
+alias gll="git log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat"
+alias gfp="git fetch --prune"
+
+# aliases
+alias rm="rm -rf"
+alias cp="cp -r"
+
